1e9a6a2a1dbfd6d4f8e3cec729a195b78dec3560
Ethanf25 <ethanf25@sbcglobal.net>
2025-06-04 18:18:47 -0400

    Minor Fix of the NavBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,16 +5,24 @@ import "../App.css"; // Import your CSS file for styling
 // Define the Header component
 const Header: React.FC = () => ( // This is a functional component that returns the header and navigation bar
   <Navbar bg="dark" variant="dark" expand="lg" className="main-header"> 
+    {/* Navbar.Brand is usually the app name or logo, linking to home */}
     <Navbar.Brand className='navbar-title' as={Link} to="/">
+      Game Library Tracker {/* Visible app name inside the brand */}
     </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
+
+    <Navbar.Toggle aria-controls="basic-navbar-nav" /> {/* Button to toggle navbar on small screens */}
+
+    <Navbar.Collapse id="basic-navbar-nav"> {/* Container for collapsible navbar content */}
       <Nav>
+        {/* Navigation links with visible text */}
         <Nav.Link className='navbar-home' as={Link} to="/">
+          Home
         </Nav.Link>
         <Nav.Link className='navbar-addgame' as={Link} to="/add">
+          Add Game
         </Nav.Link>
         <Nav.Link className='navbar-about' as={Link} to="/about">
+          About
         </Nav.Link>
       </Nav>
     </Navbar.Collapse>
